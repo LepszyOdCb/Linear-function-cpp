4fd7a5ffc3cf3f3ca0f5ed2e90e31ce4de5dd472
LepszyOdCb <135897552+LepszyOdCb@users.noreply.github.com>
2023-10-30 18:14:56 +0100

    Update Projekt Funkcje.cpp

--- Projekt Funkcje.cpp
+++ Projekt Funkcje.cpp
@@ -4,168 +4,155 @@
 using namespace std;
 
 // Funkcja liczaca a i b
-void liczenieAiB(double x1, double y1, double x2, double y2, double& a, double& b, double zmienna) {
-    zmienna = y2;
-	if (x1 == x2 && y1 == y2) {
+void liczenieAiB(double x1, double y1, double x2, double y2, double &a, double &b)
+{
+    if (x1 == x2 && y1 == y2)
+    {
         cout << "Podane wspolrzedne sa punktem!" << endl;
-    } 
-	else if (x1 != x2 && y1 != y2) {
+    }
+    else if (x1 != x2)
+    {
         a = (y2 - y1) / (x2 - x1);
         b = y1 - (a * x1);
-        if (b > 0) {
-            cout << "f(x)=" << a << "x+" << b << endl;
-        } else if (b < 0) {
-            cout << "f(x)=" << a << "x" << b << endl;
-        } else {
-            cout << "f(x)=" << a << "x" << endl;
+        if (a == 0) {
+            if (b > 0)
+            {
+                cout << "f(x) = " << abs(a) << "x + " << b << endl;
+            }
+            else if (b < 0)
+            {
+                cout << "f(x) = " << abs(a) << "x" << b << endl;
+            }
+            else
+            {
+                cout << "f(x) = " << abs(a) << "x" << endl;
+            }
+        }
+        else if (b > 0)
+        {
+            cout << "f(x) = " << a << "x + " << b << endl;
+        }
+        else if (b < 0)
+        {
+            cout << "f(x) = " << a << "x" << b << endl;
+        }
+        else
+        {
+            cout << "f(x) = " << a << "x" << endl;
         }
     }
 }
 
 // Funkcja sprawdzajaca czy funkcja rosnie lub maleje
-void monotonicznosc(double a, double x1, double y1, double x2, double y2) {
-    if (a > 0) {
+void monotonicznosc(double a)
+{
+    if (a > 0)
+    {
         cout << "Funkcja rosnie" << endl;
-    } else if (a < 0) {
+    }
+    else if (a < 0)
+    {
         cout << "Funkcja maleje" << endl;
-    } else {
+    }
+    else if (a == 0) {
+        cout << "Funkcja jest stala" << endl;
+    }
+    else
+    {
         cout << "Funkcja jest nieprawidlowa" << endl;
-	}
+    }
 }
 
 // Funkcja liczaca dziedzine oraz zbior wartosci
-void dziedzinaZbiorWartosci(double x1, double y1, double x2, double y2) {
-	if (x1 < x2) {
-		cout << "Df x = <" << x1 << ", " << x2 << ">" << endl;
-	}
-	else if (x1 > x2) {
-		cout << "Df x= <" << x2 << ", " << x1 << ">" << endl;
-	}
-	else {
-		cout << "Df x = " << x1 << endl;
-	}
-	
-	if (y1 < y2) {
-		cout << "Zw = <" << y1 << ", " << y2 << ">" << endl;
-	}
-	else if (y1 > y2) {
-		cout << "Zw = <" << y2 << ", " << y1 << ">" << endl;
-	}
-	else {
-		cout << "Zw = " << y1 << endl;
-	} 
+void dziedzinaZbiorWartosci(double a, double b, double x1, double x2)
+{
+    if (a == 0 && x1 != x2) {
+        cout << "Df: x = R" << endl;
+    }
+    else if (a != 0){
+        cout << "Df: x = R" << endl;
+    }
+    else {
+        cout << "Brak Df" << endl;
+    }
+
+    if (a == 0 && x1 != x2) {
+        cout << "Zw = " << b << endl;
+    }
+    else if (a != 0) {
+        cout << "Zw = R" << endl;
+    }
+    else {
+        cout << "Brak Zw" << endl;
+    }
 }
 
 // Funkcja liczaca dla ktorych x y jest ujemne i dodatnie
-void yUjemneDodatnieX(double a, double b, double x1, double y1, double x2, double y2, double miejscezerowe) {
-   miejscezerowe = b*(-1)/a;
-		if (x1 <= miejscezerowe && x2 <= miejscezerowe) {
-			if (x1 < x2) {
-				if (miejscezerowe == x1) {
-					cout << "Y ujemne dla x = <" << x2 << ", " << miejscezerowe << ")" << endl;
-				}
-				else if (miejscezerowe == x2) {
-					cout << "Y ujemne dla x = <" << x1 << ", " << miejscezerowe << ")" << endl;
-				}
-				else {
-					cout << "Y ujemne dla x = <" << x1 << ", " << x2 << ">" << endl;
-				}
-			}				
-			else if (x2 < x1) {
-				if (miejscezerowe == x1) {
-					cout << "Y ujemne dla x = <" << x2 << ", " << miejscezerowe << ")" << endl;
-				}
-				else if (miejscezerowe == x2) {
-					cout << "Y ujemne dla x = <" << x1 << ", " << miejscezerowe << ")" << endl;
-				}
-				else {
-					cout << "Y ujemne dla x = <" << x1 << ", " << x2 << ">" << endl;
-				}
-			}
-		}
-		else if (x1 >= miejscezerowe && x2 >= miejscezerowe) {
-			if (x1 < x2) {
-				if (miejscezerowe == x1) {
-					cout << "Y dodatnie dla x = <" << miejscezerowe << ", " << x2 << ")" << endl;
-				}
-				else if (miejscezerowe == x2) {
-					cout << "Y dodatnie dla x = <" << miejscezerowe << ", " << x1 << ")" << endl;
-				}
-				else {
-					cout << "Y dodatnie dla x = <" << x1 << ", " << x2 << ">" << endl;
-				}
-			}				
-			else if (x2 < x1) {
-				if (miejscezerowe == x1) {
-					cout << "Y dodatnie dla x = <" << miejscezerowe << ", " << x2 << ")" << endl;
-				}
-				else if (miejscezerowe == x2) {
-					cout << "Y dodatnie dla x = <" << miejscezerowe << ", " << x1 << ")" << endl;
-				}
-				else {
-					cout << "Y dodatnie dla x = <" << x2 << ", " << x1 << ">" << endl;
-				}
-			}
-		}
-		else {
-			if (x2 < x1) {
-				cout << "Y ujemne dla x = <" << x2 << ", " << miejscezerowe << ")" << endl;
-				cout << "Y dodatnie dla x = (" << miejscezerowe << ", " << x1 << ">" << endl;
-				}
-			else if (x2 > x1) {
-				cout << "Y ujemne dla x = <" << x1 << ", " << miejscezerowe << ")" << endl;
-				cout << "Y dodatnie dla x = (" << miejscezerowe << ", " << x2 << ">" << endl;	
-			}
-		}
+void yUjemneDodatnieX(double a, double b, double x1, double x2)
+{
+    double miejscezerowe = -b / a;
+    if (a == 0 && x1 != x2) {
+        if (b < 0) {
+            cout << "Y ujemne dla x = R" << endl;
+        }
+        else if (b > 0) {
+            cout << "Y dodatnie dla x = R" << endl;
+        }
+    }
+    else if (x1 != x2) {
+        cout << "Y ujemne dla x = ( -nieskonczonosc, " << miejscezerowe << " )" << endl;
+        cout << "Y dodatnie dla x = < " << miejscezerowe << ", nieskonczonosc )" << endl;       
+    } 
 }
 
 // Funkcja liczaca miejsce zerowe
-void miejscazerowe(double a, double b, double x1, double y1, double x2, double y2, double miejscezerowe) {
-   	miejscezerowe = b*(-1)/a;
-	if (x1 == miejscezerowe || x2 == miejscezerowe) {
-		cout << "Miejsce zerowe: " << miejscezerowe << endl;
-	}
-	else if (((x1>=0&&x2>=0) || (x1<=0&&x2<=0)) || (miejscezerowe>x1)&&(miejscezerowe>x2) || (miejscezerowe<x1)&&(miejscezerowe<x2)) {
-		cout << "Brak miejsc zerowych!" << endl;		
-   	}
-	else {
-		cout << "Miejsce zerowe: " << miejscezerowe << endl;
-   }
-   
+void miejscazerowe(double a, double b, double &przeciecieoy, double x1, double x2, double &miejscezerowe)
+{
+    if (a == 0 && x1 == x2) {
+        if (b == 0 && x1 != x2) {
+            cout << "Miejsce zerowe = R" << endl;
+        }
+        else if (b != 0) {
+            cout << "Przeciecie z OY = " << b << endl;
+        }
+    }
+    else if (a != 0) {
+        miejscezerowe = -b / a;
+        cout << "Miejsce zerowe = " << miejscezerowe << endl;
+        cout << "Przeciecie z OY = " << b << endl;
+    }
+    else if (a == 0 && b == 0) {
+        cout << "Miejsce zerowe = 0" << endl;
+        cout << "Przeciecie z OY = 0" << endl;
+    }
 }
 
-// Funkcja liczaca argumenty dla wartoœci
-void liczenieArgDlaWar(double a, double b, int wybor, double x, double y, bool petla, int kontynuacja, double x1, double y1, double x2, double y2) {
-	cout << "Podaj wartosc (y): " << endl;
-	cin >> y;
-	x = (y - b) / a;
-	if ((x<x1&&x<x2) || (x>x1&&x>x2)) {
-		cout << "Poza dziedzina!" << endl;
-	}
-	else {
-		cout << y <<" = f(" << x << ")" <<endl;	
-	}	
+// Funkcja liczaca argumenty dla wartosci
+void liczenieArgDlaWar(double a, double b)
+{
+    double x, y;
+    cout << "Podaj wartosc (y): " << endl;
+    cin >> y;
+    x = (y - b) / a; 
+    cout << y << " = f(" << x << ")" << endl;
 }
 
-// Funkcja liczaca argumenty dla wartoœci
-void liczenieWarDlaArg(double a, double b, int wybor, double x, double y, bool petla, int kontynuacja, double x1, double y1, double x2, double y2, double zmienna) {
-	cout << "Podaj argument (x): " << endl;
-	cin >> x;
-	y = (a*x) + b;
-	if ((y>=y1&&y<=zmienna) || (y>=zmienna&&y<=y1)) {
-		cout << "f(" << x << ") = " << y <<endl;
-	}
-	else {
-		cout << "Poza dziedzina!" << endl;
-	}	
+// Funkcja liczaca argumenty dla wartosci
+void liczenieWarDlaArg(double a, double b)
+{
+    double x, y;
+    cout << "Podaj argument (x): " << endl;
+    cin >> x;
+    y = (a * x) + b;
+    cout << "f(" << x << ") = " << y << endl;
 }
 
-// Glówna petla kodu
-int main() {
-	
+// GlÃ³wna petla kodu
+int main()
+{
     // Zmienne
-    double x1, y1, x2, y2, a = 0, b = 0, x, y, miejscezerowe, zmienna;
-    int wybor, kontynuacja;
+    double x1, y1, x2, y2, a = 0, b = 0, przeciecieoy = 0, miejscezerowe;
+    int wybor;
     bool petla = true;
 
     // Glowny kod
@@ -178,37 +165,41 @@ int main() {
     cin >> x2;
     cin >> y2;
     cout << "=-=-=-=-= Wyniki =-=-=-=" << endl;
-	liczenieAiB(x1, y1, x2, y2, a, b, zmienna);
-	monotonicznosc(a, x1, x2, y1, y2);	
-	dziedzinaZbiorWartosci(x1, y1, x2, y2);
-	yUjemneDodatnieX(a, b, x1, y1, x2, y2, miejscezerowe);
-	miejscazerowe(a, b, x1, y1, x2, y2, miejscezerowe);
-	while (petla) {
-        cout << "Wybierz opcje: " << endl;
-        cout << "- 1 - liczenie argumentu dla wartosci" << endl;
-        cout << "- 2 - liczenie wartosci dla argumentu" << endl;
-        cout << "- 3 - zakoncz dzialanie programu" << endl;
-        cin >> wybor;
+    liczenieAiB(x1, y1, x2, y2, a, b);
+    monotonicznosc(a);
+    dziedzinaZbiorWartosci(a, b, x1, x2);
+    yUjemneDodatnieX(a, b, x1, x2);
+    miejscazerowe(a, b, przeciecieoy, x1, x2, miejscezerowe);
+    
+    if (x1 != x2) {
+        while (petla)
+        {
+            cout << "Wybierz opcje: " << endl;
+            cout << "- 1 - liczenie argumentu dla wartosci" << endl;
+            cout << "- 2 - liczenie wartosci dla argumentu" << endl;
+            cout << "- 3 - zakoncz dzialanie programu" << endl;
+            cin >> wybor;
 
-        switch (wybor) {
-            case 1:
-                liczenieArgDlaWar(a, b, wybor, x, y, petla, kontynuacja, x1, y2, x2, y2);
-                break;
-            case 2:
-                liczenieWarDlaArg(a, b, wybor, x, y, petla, kontynuacja, x1, y2, x2, y2, zmienna);
-                break;
+            switch (wybor)
+            {
+                case 1:
+                    liczenieArgDlaWar(a, b);
+                    break;
+                case 2:
+                    liczenieWarDlaArg(a, b);
+                    break;
 
-            case 3:
-                petla = false;  
-                break;
+                case 3:
+                    petla = false;
+                    break;
 
-            default:
-                cout << "Niepoprawny wybor." << endl;
-                break;
+                default:
+                    cout << "Niepoprawny wybor." << endl;
+                    break;
+            }
         }
     }
-	cout << "=-=-=-=-= Koniec =-=-=-=" << endl;
-    	
-		return 0;
-}
+    cout << "=-=-=-=-= Koniec =-=-=-=";
 
+    return 0;
+}
